65985d833099cc956cc1c5d69a13490f109b0a88
Musab <musabpirzada1@gmail.com>
2024-07-16 16:43:17 +0500

    Blockchain.js file update(4)

--- Blockchain Network/blockchain.js
+++ Blockchain Network/blockchain.js
@@ -4,17 +4,35 @@ class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
     }
-    createGenesisBlock(){
+
+    createGenesisBlock() {
         return new Block("Genesis Block", "0");
     }
-    getLatestBlock(){
+
+    getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().toHash();
         newBlock.hash = newBlock.toHash();
         this.chain.push(newBlock);
+    }
 
+    isValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.previousHash !== previousBlock.toHash()) {
+                return false;
+            }
+
+            if (currentBlock.toHash() !== currentBlock.hash) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
